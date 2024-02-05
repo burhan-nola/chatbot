e2157089cd09378c08adb587bfe175a10066daa7
burhan-nola <burhan.nola@gmail.com>
2024-02-05 15:42:31 +0700

    Nola Chatbot

---
+++ script.js
@@ -0,0 +1,39 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", getResponse);
+
+async function getResponse() {
+  var inputText = document.getElementById("input").value;
+  const parentDiv = document.getElementById("chat-area");
+
+  if (inputText === "") {
+    return;
+  }
+
+  const question = document.createElement("div");
+  question.innerHTML = inputText;
+  question.classList.add("box");
+  parentDiv.appendChild(question);
+
+  document.getElementById("input").value = "";
+
+  let res = await fetch("http://localhost:5000/chatbot", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: inputText,
+    }),
+  });
+
+  const data = await res.json();
+
+  if (data) {
+    const answer = document.createElement("div");
+    answer.innerHTML = data;
+    answer.classList.add("box", "answer");
+    parentDiv.appendChild(answer);
+  }
+  // The remaining code goes inside this function
+}
